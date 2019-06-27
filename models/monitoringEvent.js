410b34a17ddad47cf285c63532bc2ee0cb407e7a
matheusbsilva <matheusbattista@hotmail.com>
2019-06-26 22:31:21 -0300

    feat(#2): create monitoring event model

---
+++ models/monitoringEvent.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MonitoringEventSchema = new Schema({
+  studentId: { type: String, required: true },
+  monitorId: { type: String, required: true },
+  discipline: { 
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    class: { type: String, required: true }
+  },
+  place: { type: String, required: true }
+  datetime: { type: Date, required: true }
+}, { collection: 'monitoringEvent' });
+
+module.exports = mongoose.model('MonitoringEventModel', MonitoringEventSchema);
